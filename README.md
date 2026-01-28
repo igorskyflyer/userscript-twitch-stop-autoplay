a0146361768d0f21bca647f2dffd3690a2fb0d8f
Igor Dimitrijević <igor.dvlpr@gmail.com>
2026-01-28 21:29:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+<div align="center">
+  <img src="https://raw.githubusercontent.com/igorskyflyer/{{repo}}/main/media/script.png" alt="" width="256" height="256">
+  <h1></h1>
+</div>
+
+<br>
+
+<h4 align="center">
+  A UserScript that {{desc}}.
+</h4>
+
+<br>
+
+## ✅ Features
+
+
+<br>
+
+## 🧩 Compatibility
+- Chrome
+- Firefox
+- Safari
+- Edge
+- Opera
+
+<br>
+
+## 📜 License
+Licensed under the [**GPL-3.0-or-later**](https://github.com/igorskyflyer/{{repo}}/blob/main/LICENSE).
+
+---
+
+Made with care by [**Igor Dimitrijević (igorskyflyer)**](https://github.com/igorskyflyer).
---
+++ script.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name
+// @namespace    https://greasyfork.org/users/1002054-igorskyflyer
+// @version      v1.0.0
+// @description  {{desc}}
+// @author       igorskyflyer
+// @date         2026--
+// @match        https://
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=
+// @run-at       document-end
+// @grant        none
+// @license      GPL-3.0-or-later
+// @compatible   chrome
+// @compatible   firefox
+// @compatible   opera
+// @compatible   safari
+// @compatible   edge
+// @homepageURL  https://github.com/igorskyflyer/{{repo}}
+// @supportURL   https://greasyfork.org/users/1002054-igorskyflyer
+// ==/UserScript==
+
+// @ts-nocheck
+
+;(() => {
+
+  }
+})()
